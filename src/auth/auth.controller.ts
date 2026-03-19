98feb28fdc2987c31605b4deeb75cda4ec301eef
João <joaovictorqueiroz39@gmail.com>
2026-03-18 21:29:25 -0300

    feat/lógica inicial de cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Request } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { CreateAnimadorDto } from 'src/animadores/dto/create-animador.dto';
 import { AuthService } from './auth.service';
 import { Public } from 'src/is-public.decorator';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -15,8 +16,20 @@ export class AuthController {
 
   @Public()
   @Post('signin')
-  async signin(@Body() body: { email: string; password: string }) {
-    return this.authService.signIn(body.email, body.password);
+  async signin(
+    @Body() body: { email: string; password: string },
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const tokens = await this.authService.signIn(body.email, body.password);
+
+    res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return { accessToken: tokens.accessToken };
   }
 
   @Public()
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,13 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
